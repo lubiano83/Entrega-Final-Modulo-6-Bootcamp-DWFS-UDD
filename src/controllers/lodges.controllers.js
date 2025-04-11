9ad1effeadd7277cf0196104c3b9d97400e5ba10
lubiano83 <josepablolubiano@lubiano83.local>
2025-04-11 16:04:36 -0400

    router de lodges terminado

--- src/controllers/lodges.controllers.js
+++ src/controllers/lodges.controllers.js
@@ -34,11 +34,23 @@ export default class LodgesControllers {
         }
     };
 
+    getLodgeById = async(req, res) => {
+        try {
+            const { id } = req.params;
+            const lodge = await lodgesDao.getLodgesById(id);
+            return res.status(200).send({ message: "Lodge obtenido por el id..", payload: lodge });
+        } catch (error) {
+            return res.status( 500 ).send({ message: "Error al obtener datos desde el servidor..", error: error.message });
+        }
+    };
+
     createLodge = async(req, res) => {
         try {
             const { hotel, size, bedroom, bathroom, capacity, season, available } = req.body;
             const { high, medium, low } = season;
+            if(!hotel || !size || !bedroom || !bathroom || !capacity || !high || !medium || !low) return res.status(404).send({ message: "Todos los campos son requeridos.." });
             const lodgeCreated = { hotel: hotel.toLowerCase(), size: Number(size), bedroom: Number(bedroom), bathroom: Number(bathroom), capacity: Number(capacity), season: { high: Number(high), medium: Number(medium), low: Number(low) }, available: Boolean(available) };
+            if(isNaN(Number(size)) || isNaN(Number(bedroom)) || isNaN(Number(bathroom)) || isNaN(Number(capacity)) || isNaN(Number(high)) || isNaN(Number(medium)) || isNaN(Number(low))) return res.status(400).send({ message: "El campo: size, bedrrom, bathrrom, capacity, high, medium, low, deben ser tipo numero.." });
             await lodgesDao.createLodge(lodgeCreated);
             return res.status(200).send({ message: "Lodge creado con exito.." });
         } catch (error) {
@@ -69,7 +81,51 @@ export default class LodgesControllers {
             await lodgesDao.updateLodgeById(id, { image: lodge.image });
             return res.status(200).send({ message: "Imagenes eliminadas con exito.." });
         } catch (error) {
-            
+            return res.status( 500 ).send({ message: "Error al obtener datos desde el servidor..", error: error.message });
+        }
+    };
+
+    updateLogdeById = async(req, res) => {
+        try {
+            const { id } = req.params;
+            const lodge = await lodgesDao.getLodgesById(id);
+            if(!lodge) return res.status(400).send({ message: "Ese lodge no existe.." });
+            const { hotel, size, bedroom, bathroom, capacity, wifi, season } = req.body;
+            const { high, medium, low } = season;
+            if(!hotel || !size || !bedroom || !bathroom || !capacity || !wifi || !high || !medium || !low) return res.status(404).send({ message: "Todos los campos son requeridos.." });
+            const updatedWifi = String(wifi).toLowerCase().trim() === "true" ? true : String(wifi).toLowerCase().trim() === "false" ? false : wifi;
+            if(typeof updatedWifi !== "boolean") return res.status(400).send({ message: "El campo wifi debe ser true o false.." });
+            const updatedLodge = { hotel: hotel.toLowerCase().trim(), size: Number(size), bedroom: Number(bedroom), bathroom: Number(bathroom), capacity: Number(capacity), wifi: updatedWifi, season: { high: Number(high), medium: Number(medium), low: Number(low) }};
+            if(isNaN(Number(size)) || isNaN(Number(bedroom)) || isNaN(Number(bathroom)) || isNaN(Number(capacity)) || isNaN(Number(high)) || isNaN(Number(medium)) || isNaN(Number(low))) return res.status(400).send({ message: "El campo: size, bedrrom, bathrrom, capacity, high, medium, low, deben ser tipo numero.." });
+            await lodgesDao.updateLodgeById(id, updatedLodge);
+            return res.status(200).send({ message: "Lodge actualizado con exito.." });
+        } catch (error) {
+            return res.status( 500 ).send({ message: "Error al obtener datos desde el servidor..", error: error.message });
+        }
+    };
+
+    changeAvailableById = async(req, res) => {
+        try {
+            const { id } = req.params;
+            const lodge = await lodgesDao.getLodgesById(id);
+            if(!lodge) return res.status(400).send({ message: "Ese lodge no existe.." });
+            const changeAvailable = { available: !lodge.available };
+            await lodgesDao.updateLodgeById(id, changeAvailable);
+            return res.status(200).send({ message: "Available cambiado con exito.." });
+        } catch (error) {
+            return res.status( 500 ).send({ message: "Error al obtener datos desde el servidor..", error: error.message });
+        }
+    };
+
+    deleteLodgeById = async(req, res) => {
+        try {
+            const { id } = req.params;
+            const lodge = await lodgesDao.getLodgesById(id);
+            if(!lodge) return res.status(400).send({ message: "Ese lodge no existe.." });
+            await lodgesDao.deleteLodgeById(id);
+            return res.status(200).send({ message: "Lodge eliminado con exito.." });
+        } catch (error) {
+            return res.status( 500 ).send({ message: "Error al obtener datos desde el servidor..", error: error.message });
         }
     };
 
--- src/routes/lodges.router.js
+++ src/routes/lodges.router.js
@@ -7,7 +7,11 @@ const lodgesControllers = new LodgesControllers();
 ROUTER.get("/", lodgesControllers.getLodges);
 ROUTER.post("/", lodgesControllers.createLodge);
 ROUTER.delete("/", lodgesControllers.deleteAllLodges);
+ROUTER.get("/:id", lodgesControllers.getLodgeById);
+ROUTER.put("/:id",lodgesControllers.updateLogdeById);
 ROUTER.patch("/:id", lodgesControllers.addImageToLodge);
+ROUTER.delete("/:id", lodgesControllers.deleteLodgeById);
 ROUTER.delete("/image/:id", lodgesControllers.deleteAllImageFromLodge);
+ROUTER.patch("/available/:id", lodgesControllers.changeAvailableById);
 
 export default ROUTER;
