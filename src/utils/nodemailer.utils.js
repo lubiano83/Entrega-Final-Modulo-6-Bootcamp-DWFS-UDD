920a61599cd659462562a06b56dfa32d00328154
lubiano83 <josepablolubiano@lubiano83.local>
2025-04-13 19:33:50 -0400

    ajustando envio de mail

--- src/utils/nodemailer.utils.js
+++ src/utils/nodemailer.utils.js
@@ -3,7 +3,7 @@ import moment from "moment";
 
 export const sendReservationEmail = async (reservation) => {
     try {
-        const imageUrl = `https://backend-sistema-de-reservas.onrender.com/logo.webp`;
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/lasTrancasLodges%2Flogo.webp?alt=media&token=0c5d44ca-1507-4765-b55e-6209e6041bf5`;
         const mailOptions = {
             from: `"Reservas Cabañas" <${process.env.MAIL_USER}>`,
             to: reservation.email,
@@ -11,12 +11,12 @@ export const sendReservationEmail = async (reservation) => {
             html: `
                 <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center; padding: 0px 30px 0px 30px;">
                     <div style="width: 100%; display: flex; justify-content: center; align-items: center;">
-                        <img src="${imageUrl}" alt="Logo Cabañas" style="width: 150px; height: auto;">
+                        <img src="${imageUrl}" alt="Logo Cabañas" style="width: 200; height: auto;">
                     </div>
                     <div style="display: flex; flex-direction: column; align-items: start;">
                         <h2>Hola ${reservation.name}, tu reserva ha sido confirmada.</h2>
-                        <p><strong>Fecha de llegada:</strong> ${moment(reservation.arrive).format("DD/MM/YYYY")}</p>
-                        <p><strong>Fecha de salida:</strong> ${moment(reservation.leave).format("DD/MM/YYYY")}</p>
+                        <p><strong>Fecha de llegada:</strong> ${moment(reservation.arrive).format("DD/MM/YYYY")} desde las 16:00 hrs.</p>
+                        <p><strong>Fecha de salida:</strong> ${moment(reservation.leave).format("DD/MM/YYYY")} a las 12:00 hrs maximo.</p>
                         <p><strong>Cabaña:</strong> ${reservation.lodge}</p>
                         <p><strong>Personas:</strong> ${reservation.people}</p>
                         <p><strong>Total a pagar:</strong> $${reservation.price}</p>
@@ -27,10 +27,8 @@ export const sendReservationEmail = async (reservation) => {
         };
 
         const info = await transporter.sendMail(mailOptions);
-        console.log("Correo enviado:", info.messageId);
         return { success: true, messageId: info.messageId };
     } catch (error) {
-        console.error("Error enviando email:", error);
         return { success: false, error: error.message };
     }
 };
